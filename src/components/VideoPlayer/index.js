7ef8cc78550ff32aad3790dd6ef8f116b6f4afda
VENKEYY78 <jvenkeyy78@gmail.com>
2025-07-01 11:10:06 +0000

    added play and pause button

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,14 +1,37 @@
+import {Component} from 'react'
+
 import './index.css'
 
 import ReactPlayer from 'react-player'
 
-const VideoPlayer = () => (
-  <div className="video-container">
-    <h1 className="heading">Video Player</h1>
-    <div className="responsive-container">
-      <ReactPlayer url="https://youtu.be/BtrXJqykU-0?si=hwfVWfhxF0OCNDBl" />
-    </div>
-  </div>
-)
+const videoURL = 'https://youtu.be/BtrXJqykU-0?si=hwfVWfhxF0OCNDBl'
+
+class VideoPlayer extends Component {
+  state = {
+    isPlaying: false,
+  }
+
+  onClickPlay = () => {
+    this.setState(prevState => ({
+      isPlaying: !prevState.isPlaying,
+    }))
+  }
+
+  render() {
+    const {isPlaying} = this.state
+    const btnText = isPlaying ? 'Pause' : 'Play'
+    return (
+      <div className="video-container">
+        <h1 className="heading">Video Player</h1>
+        <div className="responsive-container">
+          <ReactPlayer url={videoURL} playing={isPlaying} />
+        </div>
+        <button type="button" className="button" onClick={this.onClickPlay}>
+          {btnText}
+        </button>
+      </div>
+    )
+  }
+}
 
 export default VideoPlayer
